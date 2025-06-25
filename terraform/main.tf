c17167eeab31c842dd2c78e774f602646852dcb1
parthi-kk <parthiban1612@gmail.com>
2025-06-25 20:11:38 +0530

    adding the terraform and workflow files

---
+++ terraform/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  required_version = ">= 1.4.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "demo_bucket" {
+  bucket        = "terra-github"
+  force_destroy = true
+}
