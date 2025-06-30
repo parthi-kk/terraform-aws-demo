0958353c105dbdb7e6d5e4383f918f0388db493e
parthi-kk <66585123+parthi-kk@users.noreply.github.com>
2025-06-30 11:17:50 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "demo-learn1" {
+resource "aws_s3_bucket" "mirshi" {
   bucket        = "rockstar-mirshi"
   force_destroy = true
 }
