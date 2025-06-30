8631f585e208e086ef4bd09c02cc75ec6d1d4bd9
parthi-kk <parthiban1612@gmail.com>
2025-06-30 10:41:20 +0530

    terraform state bucket

---
+++ terraform/bootstrap/main.tf
@@ -0,0 +1,37 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "tf_state_bucket" {
+  bucket        = "terra-state-bucket"
+  force_destroy = true
+
+  tags = {
+    Name        = "Terraform State Bucket"
+    Environment = "Bootstrap"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.tf_state_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "tf_lock_table" {
+  name         = "terraform-lock-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "Terraform Lock Table"
+    Environment = "Bootstrap"
+  }
+}
